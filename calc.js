a72347000b484c6a37351618d14e904e6fb7c688
Marc Garreau <marcdgarreau@gmail.com>
2014-11-03 11:56:59 -0700

    Basic calculator functionality

--- calc.js
+++ calc.js
@@ -1,2 +1,22 @@
+var keys = document.querySelectorAll('span')
 
-alert("JS is live");
+for(var i = 0; i < keys.length; i++) {
+  keys[i].onclick = function(e) {
+    var input    = document.querySelector('.screen');
+    var inputVal = input.innerHTML;
+    var btnVal   = this.innerHTML;
+
+    if (btnVal == "C") {
+      input.innerHTML = "";
+    }
+    else if (btnVal == '=') {
+      var equation = inputVal;
+      if(equation) {
+        input.innerHTML = eval(equation);
+      }
+    }
+    else {
+      input.innerHTML += btnVal;
+    }
+  }
+}
